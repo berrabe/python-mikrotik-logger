366dd3e419e383f8e49ea6605093977cb60fb22d
berrabe@manjaro <rizalical250@gmail.com>
2020-12-09 15:26:10 +0700

    1 - update main.py   - add improvement for multiple yaml block,     now this script can use multiple config

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
 	try:
+
 		logging.basicConfig(
 			level = logging.INFO,
 			filename='python-mikrotik-logger.log', filemode='a',
@@ -26,30 +27,33 @@ if __name__ == '__main__':
 
 
 		with open('hosts.yml') as yaml_:
-			conf = yaml.load(yaml_, Loader=yaml.FullLoader).get('mtk_devices')
-			hosts = conf['hosts']
-			patterns = conf['patterns']
-			var = conf['vars']
+			confs = yaml.load_all(yaml_, Loader=yaml.FullLoader)
+
+			for conf in confs:
+				conf = conf.get('mtk_devices')
+
+				hosts = conf['hosts']
+				patterns = conf['patterns']
+				var = conf['vars']
 
 
-		for host in hosts.keys():
+				for host in hosts.keys():
+					start_time = time.time()
+					logger.info("================== Logger Start @ %s ==================", ''.join(hosts[host]['mtk_host'][:]))
 
-			start_time = time.time()
-			logger.info("================== Logger Start @ %s ==================", ''.join(hosts[host]['mtk_host'][:]))
+					MikrotikLogger = mikrotik_.MikrotikLogger(
+						pattern = patterns,
+						host = hosts[host]['mtk_host'],
+						port = hosts[host]['mtk_port'],
+						username = hosts[host]['mtk_username'],
+						password = hosts[host]['mtk_password'])
 
-			MikrotikLogger = mikrotik_.MikrotikLogger(
-				pattern = patterns,
-				host = hosts[host]['mtk_host'],
-				port = hosts[host]['mtk_port'],
-				username = hosts[host]['mtk_username'],
-				password = hosts[host]['mtk_password'])
+					Notif = notif_.Notif(host = hosts[host]['mtk_host'])
+					Notif.telegram_notif(
+						token = var['telegram_token'],
+						chatid = var['telegran_chatid'])
 
-			Notif = notif_.Notif(host = hosts[host]['mtk_host'])
-			Notif.telegram_notif(
-				token = var['telegram_token'],
-				chatid = var['telegran_chatid'])
-
-			logger.info("================== Done @ %.2f Second ==================\n\n", time.time() - start_time)
+					logger.info("================== Done @ %.2f Second ==================\n\n", time.time() - start_time)
 
 	except KeyboardInterrupt:
 		sys.exit(17)
